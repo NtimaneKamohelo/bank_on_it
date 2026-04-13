eac75069da721074bda166e1652e700cf0d39344
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2026-04-13 22:45:50 +0200

    created new springboot application with dependencies in place

---
+++ bankOnIt/src/main/java/com/kamo/bankOnIt/BankOnItApplication.java
@@ -0,0 +1,13 @@
+package com.kamo.bankOnIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOnItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOnItApplication.class, args);
+	}
+
+}
---
+++ bankOnIt/src/test/java/com/kamo/bankOnIt/BankOnItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamo.bankOnIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOnItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
