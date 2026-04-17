0febae7aa19fdacc4db961e0a7b244789d656269
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2026-04-17 12:48:30 +0200

    Developed an EmailService: to send authentication emails

--- bankOnIt/src/main/java/com/kamo/bankOnIt/service/AuthService.java
+++ bankOnIt/src/main/java/com/kamo/bankOnIt/service/AuthService.java
@@ -1,4 +1,38 @@
 package com.kamo.bankOnIt.service;
 
+
+import com.kamo.bankOnIt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final EmailService emailService;
+
+    public AuthService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            AuthenticationManager authenticationManager,
+            EmailService emailService
+    ){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.emailService = emailService;
+    }
+
+    /** LOGIN */
+
+    /**Verify User*/
+
+    /** Resend Verification Code*/
+
+    /** Send Verification email */
 }
